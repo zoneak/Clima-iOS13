b6f77636361e6aa88687e1e85034ef178c4f0e13
Adriano <adriano.kaito@gmail.com>
2020-02-10 18:38:07 -0300

    commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Adriano Kaito on 10/02/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    let description: String
+}
